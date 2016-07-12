f2adafc232dde62a79a053733fbcdb3a9e6404ad
PJ Cabrera <pj.cabrera@gmail.com>
2016-07-11 21:34:15 -0700

    Renamed the streaming scripts and added streaming-related endpoints.

--- src/lib/routes/index.js
+++ src/lib/routes/index.js
@@ -1,38 +1,43 @@
-// const childProcess = require('child_process');
+import childProcess from 'child_process';
 
-exports.serveIndex = (app, staticFolder) => {
-  /*
+exports.serveEndpoints = (app, staticFolder) => {
   app.get('/start_streaming', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('start_streaming');
-    // start ffmpeg streaming from node
-    setTimeout(() => {
-      childProcess.exec('scripts/start_ffmpeg_streaming.sh', (error, stdout, stderr) => {
-        if (error) {
-          console.log(error.stack);
-          console.log('start_ffmpeg_streaming.sh error code: ' + error.code);
-          console.log('start_ffmpeg_streaming.sh signal received: ' + error.signal);
-        }
-      });
-    }, 0);
+    startStreaming();
   });
 
   app.get('/stop_streaming', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('stop_streaming');
-    // stop ffmpeg streaming from node
-    childProcess.exec('scripts/stop_ffmpeg.sh', (error, stdout, stderr) => {
-      if (error) {
-        console.log(error.stack);
-        console.log('stop_ffmpeg.sh error code: ' + error.code);
-        console.log('stop_ffmpeg.sh signal received: ' + error.signal);
-      }
-    });
+    stopStreaming();
   });
-*/
+
   // Load the single view
   // NB: This needs to be the last route added
   app.get('*', (req, res) => {
     res.sendFile('index.html', { root: staticFolder });
   });
 };
+
+function startStreaming() {
+  setTimeout(() => {
+    childProcess.exec('scripts/start_streaming.sh', (error, stdout, stderr) => {
+      if (error && error.code !== 255) {
+        console.log(error.stack);
+        console.log('start_streaming.sh error code: ' + error.code);
+        console.log('start_streaming.sh signal received: ' + error.signal);
+      }
+    });
+  }, 0);
+}
+
+function stopStreaming() {
+  childProcess.exec('scripts/stop_streaming.sh', (error, stdout, stderr) => {
+    if (error && error.code !== 255) {
+      console.log(error.stack);
+      console.log('stop_streaming.sh error code: ' + error.code);
+      console.log('stop_streaming.sh signal received: ' + error.signal);
+    }
+  });
+}
