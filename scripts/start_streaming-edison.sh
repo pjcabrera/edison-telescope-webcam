19dfacc755c12ce4d656d88acdb6dc0d131a7d26
PJ Cabrera <pj.cabrera@gmail.com>
2016-07-13 00:10:03 -0700

    Remove internal testing scripts to avoid confusion

--- scripts/start_streaming-edison.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-~/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -f mpeg1video \
--b:v 1200k -r 30 http://127.0.0.1:8082
--- scripts/start_streaming-mac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ffmpeg -s 1280x720 -f avfoundation -i "default" -f mpeg1video \
--b:v 1200k -r 30 -mbd rd -trellis 2 -cmp 2 -subcmp 2 http://127.0.0.1:8082
