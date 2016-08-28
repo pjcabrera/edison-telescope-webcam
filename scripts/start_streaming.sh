6f84e115d9fe2a0e82104315f1bb7590366f9c1d
PJ Cabrera <pj.cabrera@gmail.com>
2016-08-27 21:52:47 -0700

    Reduce streaming resolution and bandwidth to improve stability.

--- scripts/start_streaming.sh
+++ scripts/start_streaming.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-~/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -f mpeg1video \
--b:v 1200k -r 30 http://127.0.0.1:8082
+~/bin/ffmpeg/ffmpeg -f video4linux2 -i /dev/video0 \
+-s 640x480 -r 30 \
+-f mpeg1video http://127.0.0.1:8082 >/dev/null 2>&1
